a0cede307e01049ad39bbfa43edcf0e3fd6c1edc
Baptiste <boyerbaptiste.pro@gmail.com>
2018-02-21 10:59:50 +0100

    Menu mobile

---
+++ js/clic.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $( ".logo" ).click(function() {
+    var $t = $('.nav');
+
+    if ($t.is(':visible')) {
+        $t.slideUp();
+        // Other stuff to do on slideUp
+    } else {
+        $t.slideDown();
+        // Other stuff to down on slideDown
+    }
+  });
+});
